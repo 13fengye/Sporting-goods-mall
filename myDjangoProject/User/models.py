471545b03c5e0e8cf73dd06d31fad3f6bd75443b
13fengye <pengzhentong@aragoncs.com>
2022-04-09 12:39:17 +0800

    1

--- myDjangoProject/User/models.py
+++ myDjangoProject/User/models.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 # 用户表
@@ -9,6 +7,8 @@ class User(models.Model):
     password = models.CharField(max_length=32)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=32, unique=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
     
     
     class meta:

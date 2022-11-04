15ea0499c420c11c993f36bdc969fa182a76baf5
13fengye <pengzhentong@aragoncs.com>
2022-11-04 13:13:21 +0800

    update

--- myDjangoProject/myDjangoProject/settings.py
+++ myDjangoProject/myDjangoProject/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': '199953',
+        'PASSWORD': '123456',
     }
 }
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
